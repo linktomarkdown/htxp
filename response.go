06f9f9ec544cefcddd74fd824040dbfff9bbd2d8
chenxi <chenfuqiao@zuoyebang.com>
2025-07-25 18:58:27 +0800

    feat: 响应支持自定义HTTP状态码和业务code，增加ErrorWithCode方法

--- response.go
+++ response.go
@@ -15,24 +15,29 @@ type Body struct {
 
 // Success 处理成功响应
 func Success(w http.ResponseWriter, data interface{}) {
-	Response(w, data, nil)
+	Response(w, data, nil, http.StatusOK, 200)
 }
 
-// Error 处理错误响应
+// Error 处理错误响应（默认 code -1, httpStatus 200）
 func Error(w http.ResponseWriter, err error) {
-	Response(w, nil, err)
+	Response(w, nil, err, http.StatusOK, -1)
+}
+
+// ErrorWithCode 支持自定义 httpStatus 和 code
+func ErrorWithCode(w http.ResponseWriter, err error, httpStatus, code int) {
+	Response(w, nil, err, httpStatus, code)
 }
 
 // Response 统一处理HTTP响应
-func Response(w http.ResponseWriter, resp interface{}, err error) {
+func Response(w http.ResponseWriter, resp interface{}, err error, httpStatus, code int) {
 	var body Body
 	if err != nil {
-		body.Code = -1
+		body.Code = code
 		body.Msg = err.Error()
 	} else {
-		body.Code = 200
+		body.Code = code
 		body.Msg = "success"
 		body.Data = resp
 	}
-	httpx.OkJson(w, body)
+	httpx.WriteJson(w, httpStatus, body)
 }
